6cb27f4128fad60e400f8bf023470f59f2adf1ad
Paul <github@witte-steur.nl>
2026-08-16 13:45:19 +0200

    Added Page layout component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
+  import Page from './components/layout/Page.vue';
+
   import Home from './components/icons/Home.vue';
 </script>
 
 <template>
-  <h1><Home /> Nodes</h1>
+  <Page>
+    <h1><Home /> Nodes</h1>
+  </Page>
 </template>
---
+++ src/components/layout/Page.vue
@@ -0,0 +1,26 @@
+` `
+<script lang="ts" setup>
+  import { useSlots } from 'vue';
+
+  import './Page.scss';
+
+  const slots = useSlots();
+
+  if (!slots.default) {
+    throw new Error('Page should have content in default slot');
+  }
+</script>
+
+<template>
+  <section class="Page">
+    <template v-if="slots.iconBar">
+      <section class="Page--IconBar">
+        <slot name="iconBar" />
+      </section>
+    </template>
+
+    <section class="Page--Content">
+      <slot />
+    </section>
+  </section>
+</template>
